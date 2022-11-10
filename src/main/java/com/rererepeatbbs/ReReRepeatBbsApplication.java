3317c53f0bb836878e7cebd90c67a0c7db90abc3
chordpli <chordplaylist@gmail.com>
2022-11-10 11:12:31 +0900

    feat: build project

---
+++ src/main/java/com/rererepeatbbs/ReReRepeatBbsApplication.java
@@ -0,0 +1,13 @@
+package com.rererepeatbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReReRepeatBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReReRepeatBbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rererepeatbbs/ReReRepeatBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rererepeatbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReReRepeatBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
