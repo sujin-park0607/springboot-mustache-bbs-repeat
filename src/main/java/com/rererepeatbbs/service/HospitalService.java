1f9fb4fd24a591c6f304c74be88f9d2d3778ddff
chordpli <chordplaylist@gmail.com>
2022-11-11 13:48:17 +0900

    feat: add HospitalService

---
+++ src/main/java/com/rererepeatbbs/service/HospitalService.java
@@ -0,0 +1,22 @@
+package com.rererepeatbbs.service;
+
+import com.rererepeatbbs.domain.entity.Hospital;
+import com.rererepeatbbs.repository.HospitalRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class HospitalService {
+    public final HospitalRepository hospitalRepository;
+
+    public HospitalService(HospitalRepository hospitalRepository) {
+        this.hospitalRepository = hospitalRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Hospital> pageList(Pageable pageable) {
+        return hospitalRepository.findAll(pageable);
+    }
+}
