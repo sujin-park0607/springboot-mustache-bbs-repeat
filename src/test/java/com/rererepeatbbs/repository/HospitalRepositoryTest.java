bcdfbc9542a1519775f776155c6f5868b02f8031
chordpli <chordplaylist@gmail.com>
2022-11-11 13:50:19 +0900

    test: findById test

---
+++ src/test/java/com/rererepeatbbs/repository/HospitalRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.rererepeatbbs.repository;
+
+import com.rererepeatbbs.domain.entity.Hospital;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HospitalRepositoryTest {
+
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    @Test
+    void findById(){
+        Optional<Hospital> hospital = hospitalRepository.findById(1);
+        Hospital hp = hospital.get();
+        System.out.println(hp.getId());
+        assertEquals(1, hp.getId());
+    }
+}
