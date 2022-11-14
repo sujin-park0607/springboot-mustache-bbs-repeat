610cefcb410a53e944ecba223fe271a7abe23c45
chordpli <chordplaylist@gmail.com>
2022-11-14 14:46:35 +0900

    feat: add jpql
    1. Containing
    2. StartingWith
    3. EndsWith
    4. Between
    5. In

--- src/main/java/com/rererepeatbbs/repository/HospitalRepository.java
+++ src/main/java/com/rererepeatbbs/repository/HospitalRepository.java
@@ -3,5 +3,18 @@ package com.rererepeatbbs.repository;
 import com.rererepeatbbs.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypes);
+
+    List<Hospital> findByFullAddressOrBusinessTypeName(String fullAddress, String businessTypeName);
+
+    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+
+    List<Hospital> findByHospitalNameStartingWith(String keyword);   // 시작
+
+    List<Hospital> findByHospitalNameEndsWith(String keyword); // 끝
+
+    List<Hospital> findByTotalNumberOfBedsBetween(int num1, int numb2);
 }
