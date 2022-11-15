0b9d753086ef69c2dae5d60a103c65e3fc38ef47
chordpli <chordplaylist@gmail.com>
2022-11-15 14:01:17 +0900

    feat: layered 분리

--- src/main/java/com/rererepeatbbs/controller/HospitalRestController.java
+++ src/main/java/com/rererepeatbbs/controller/HospitalRestController.java
@@ -2,7 +2,7 @@ package com.rererepeatbbs.controller;
 
 import com.rererepeatbbs.domain.dto.HospitalResponse;
 import com.rererepeatbbs.domain.entity.Hospital;
-import com.rererepeatbbs.repository.HospitalRepository;
+import com.rererepeatbbs.service.HospitalService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,19 +15,26 @@ import java.util.Optional;
 @RequestMapping("/api/v1/hospitals")
 public class HospitalRestController {
 
-    private final HospitalRepository hospitalRepository;
+    private final HospitalService hospitalService;
 
-    public HospitalRestController(HospitalRepository hospitalRepository) {
-        this.hospitalRepository = hospitalRepository;
+    public HospitalRestController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
     }
 
-    @GetMapping("/{id}")
+    /*@GetMapping("/{id}")
     public ResponseEntity<HospitalResponse> get(@PathVariable Integer id) {
-        Optional<Hospital> hospital = hospitalRepository.findById(id);    // Entity
+        Optional<Hospital> hospital = hospitalService.findById(id);    // Entity
         if (hospital.isPresent()) {
             HospitalResponse hospitalResponse = Hospital.of(hospital.get());
             return ResponseEntity.ok().body(hospitalResponse);
         }
         return null;
+    }*/
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HospitalResponse> getHospital(@PathVariable Integer id) {
+        HospitalResponse hospitalResponse = hospitalService.getHospital(id);
+        return ResponseEntity.ok().body(hospitalResponse);
     }
+
 }
