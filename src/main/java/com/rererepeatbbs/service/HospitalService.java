c56574a90dca0cd816e692c7667a5b81d4efc4a6
chordpli <chordplaylist@gmail.com>
2022-11-15 14:01:01 +0900

    feat: getHospital()
    1. layered 분리, 영업중 추가

--- src/main/java/com/rererepeatbbs/service/HospitalService.java
+++ src/main/java/com/rererepeatbbs/service/HospitalService.java
@@ -1,5 +1,6 @@
 package com.rererepeatbbs.service;
 
+import com.rererepeatbbs.domain.dto.HospitalResponse;
 import com.rererepeatbbs.domain.entity.Hospital;
 import com.rererepeatbbs.repository.HospitalRepository;
 import org.springframework.data.domain.Page;
@@ -24,5 +25,19 @@ public class HospitalService {
 
     public Optional<Hospital> showHospitalInfo(Integer id) {
         return hospitalRepository.findById(id);
+    }
+
+    public HospitalResponse getHospital(Integer id) {
+        Optional<Hospital> optHospital = hospitalRepository.findById(id);
+        Hospital hospital = optHospital.get();
+        HospitalResponse hospitalResponse = Hospital.of(hospital); // DTO
+        if (hospital.getBusinessStatusCode() == 13) {
+            hospitalResponse.setBusinessStatusName("영업중");
+        } else if (hospital.getBusinessStatusCode() == 3) {
+            hospitalResponse.setBusinessStatusName("폐업");
+        } else {
+            hospitalResponse.setBusinessStatusName(String.valueOf(hospital.getBusinessStatusCode()));
+        }
+        return hospitalResponse;
     }
 }
