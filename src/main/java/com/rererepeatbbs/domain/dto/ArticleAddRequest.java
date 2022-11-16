5cfc91c6f77405d229f8246d1170775425fae755
chordpli <chordplaylist@gmail.com>
2022-11-16 15:02:12 +0900

    feat: add ArticleAddRequest

---
+++ src/main/java/com/rererepeatbbs/domain/dto/ArticleAddRequest.java
@@ -0,0 +1,20 @@
+package com.rererepeatbbs.domain.dto;
+
+import com.rererepeatbbs.domain.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ArticleAddRequest {
+    private String title;
+    private String content;
+
+    public Article toEntity(){
+        return Article.builder()
+                .title(this.getTitle())
+                .content(this.getContent())
+                .build();
+
+    }
+}
