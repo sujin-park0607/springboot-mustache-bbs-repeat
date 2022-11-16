9a089d4c681bf580f5f230c6dc13b9971df79b27
chordpli <chordplaylist@gmail.com>
2022-11-16 15:02:28 +0900

    feat: add annotation builder

--- src/main/java/com/rererepeatbbs/domain/entity/Article.java
+++ src/main/java/com/rererepeatbbs/domain/entity/Article.java
@@ -2,6 +2,7 @@ package com.rererepeatbbs.domain.entity;
 
 import com.rererepeatbbs.domain.dto.ArticleResponse;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Builder
 @Entity
 @Getter
 @AllArgsConstructor
