97fc1e05c547e19e7347ca817aee995f118a696f
chordpli <chordplaylist@gmail.com>
2022-11-18 14:20:47 +0900

    feat: keyword 검색 추가

--- src/main/java/com/rererepeatbbs/controller/HospitalController.java
+++ src/main/java/com/rererepeatbbs/controller/HospitalController.java
@@ -3,19 +3,23 @@ package com.rererepeatbbs.controller;
 import com.rererepeatbbs.domain.entity.Hospital;
 import com.rererepeatbbs.repository.HospitalRepository;
 import com.rererepeatbbs.service.HospitalService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Controller
 @RequestMapping("/hospital")
 public class HospitalController {
@@ -26,7 +30,7 @@ public class HospitalController {
         this.hospitalService = hospitalService;
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value="/{id}")
     public String showHospitalInfo(@PathVariable Integer id, Model model) {
         Optional<Hospital> hospital = hospitalService.showHospitalInfo(id);
         if (hospital.isPresent()) {
@@ -35,10 +39,11 @@ public class HospitalController {
         }else{
             return "hospital/error";
         }
-
     }
 
-    @GetMapping("/list")
+
+
+    @GetMapping("/list2")
     public String hospitalList(Model model,
                                @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
         Page<Hospital> hospitalList = hospitalService.pageList(pageable);
@@ -50,13 +55,18 @@ public class HospitalController {
         return "hospital/list";
     }
 
-    @GetMapping("/")
+    /*@GetMapping("/")
     public String index() {
         return "redirect:./list";
-    }
+    }*/
 
     @GetMapping("")
-    public String index2() {
-        return "redirect:./list";
+    public String index2(@RequestParam String keyword, Pageable pageable, Model model) {
+        log.info("keyword:{}", keyword);
+        Page<Hospital> hospitals = hospitalService.findByRoadNameAddressContaining(keyword, pageable);
+        model.addAttribute("hospitals", hospitals);
+        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+        model.addAttribute("next", pageable.next().getPageNumber());
+        return "hospital/list";
     }
 }
